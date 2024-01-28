a8f53ef9f5ca536db13f8d7c99ae7b14bbeadbf7
YaiiGarcerant <126992301+YaiiGarcerant@users.noreply.github.com>
2024-01-28 16:29:45 -0500

    Primer Plano: Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,63 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Seguimiento | CNCA </title>
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/sweetalert2.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.min.css') }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <script src="{{ asset('js/sweetalert2.js') }}"></script>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+</head>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<body>
+    <header class="banner">
+        <div class="container">
+            <img src="{{ asset('img/logo.png') }}">
+            <h4 class="fs-5 m-2">SERVICIO NACIONAL DE APRENDIZAJE</h4>
         </div>
-    </div>
-</div>
-@endsection
+    </header>
+
+    <main class="main card-container">
+        <section class="card">
+            <img src="{{ asset('img/Ilustracion.svg') }}" alt="">
+        </section>
+
+        <section class="card form">
+            <div class="form-title">
+                <h4>CENTRO NACIONAL COLOMBO ALEMAN</h4>
+                <p>SEGUIMIENTO DE INSTRUCTORES</p>
+            </div>
+            <div class="form-content">
+                <form action="" method="post">
+                    <div class="input-with-icon input-content">
+                        <label for="" class="form-label">Usuario</label>
+                        <input type="text" placeholder="Usuario">
+                        <i class="bi bi-person"></i>
+
+                    </div>
+
+                    <div class="input-with-icon input-content">
+                        <label for="" class="form-label">Contraseña</label>
+                        <input type="text" placeholder="Contraseña">
+                        <i class="bi bi-unlock"></i>
+                    </div>
+                    <div class="input-button">
+                        <input type="submit" value="INGRESAR">
+                    </div>
+                </form>
+
+
+            </div>
+        </section>
+    </main>
+
+</body>
+
+</html>
