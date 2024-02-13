9a6cade9db151bf9094e1b25dad364e1fb546b0c
YaiiGarcerant <126992301+YaiiGarcerant@users.noreply.github.com>
2024-02-13 18:41:24 -0500

    Funcion Actualizar Fechas con calendar

--- resources/views/evaluations/create.blade.php
+++ resources/views/evaluations/create.blade.php
@@ -2,8 +2,10 @@
 @section('content')
     <div class="contenedor">
         <div class="mt-2 text-lg-center">
-            <h4 class="card-title color mb-1 text-lg-center fw-semibold">Fechas Entrega De Evaluaciones <i class="bi bi-bar-chart"></i></h4>
-            <p class="fw-semibold text-secondary mb-5">Registro automatizado de las fehcas de entrega de evaluaciones por instructor</p>
+            <h4 class="card-title color mb-1 text-lg-center fw-semibold">Fechas Entrega De Evaluaciones <i
+                    class="bi bi-bar-chart"></i></h4>
+            <p class="fw-semibold text-secondary mb-5">Registro automatizado de las fehcas de entrega de evaluaciones por
+                instructor</p>
         </div>
 
         <div class="m-2">
@@ -66,16 +68,10 @@
                 </form>
             </div>
 
-
-
-
-
-
         </div>
     </div>
 
     <script>
-        // Función para generar fechas
         function generarFechas() {
 
             var fechaInicialInput = document.getElementById("fechaInicial");
@@ -83,57 +79,37 @@
             var selectInput = document.getElementById("funcionario_id");
             var contenedorFechasGeneradasDiv = document.getElementById("contenedorFechas");
             var fechasGeneradasInput = document.getElementById('fechas_generadas');
-            var contenedor = document.getElementById('funcionario');; // Variable para almacenar el número de fechas generadas
-            var contadorFechas = 0; // Variable para almacenar el número de fechas generadas
-            var fechasGeneradas = []; // Array para almacenar las fechas generadas
+            var contenedor = document.getElementById('funcionario');
+            var contadorFechas = 0;
+            var fechasGeneradas = [];
+            var hr = document.getElementById('separador');
 
-            var hr =  document.getElementById('separador');
-
-            // Validar si el select está vacío
             if (selectInput.value.trim() === "" || selectInput.value.trim() === "" || anioInput.value.trim() === "") {
                 Swal.fire({
                     title: "Fechas No Generadas",
                     text: "Recuerdo rellenar los todos los campos",
                     icon: "error"
                 });
-
-
             } else {
-                // Obtener fecha inicial
                 var fechaInicial = new Date(fechaInicialInput.value);
                 var anioSeleccionado = parseInt(anioInput.value);
-
-                // Limpiar fechas generadas previamente
                 contenedorFechasGeneradasDiv.innerHTML = '';
-
-                // Generar fechas cada 15 días para el año seleccionado
                 var fecha = new Date(fechaInicial);
                 while (fecha.getFullYear() === anioSeleccionado) {
-
-
                     var input = document.createElement("input");
                     input.type = "date";
                     input.value = fecha.toISOString().substr(0, 10);
                     input.className = " form-control border-2 shadow-sm mb-2 w-2";
-
-                    input.id = "fecha" + contadorFechas;
-
+                    var indice = fechasGeneradas.length;
+                    input.id = "fecha" + indice;
                     var divContenedor = document.createElement("div");
                     divContenedor.className = "mt-3 d-none";
-
                     divContenedor.appendChild(input);
                     contenedorFechasGeneradasDiv.appendChild(divContenedor);
-
-                    contadorFechas++; // Incrementar el contador de fechas generadas
-                    fechasGeneradas.push(fecha.toISOString().substr(0, 10)); // Agregar la fecha al array
-
-                    // Añadir 15 días a la fecha
+                    contadorFechas++;
+                    fechasGeneradas.push(fecha.toISOString().substr(0, 10));
                     fecha.setDate(fecha.getDate() + 15);
                 }
-
-                // Mostrar el número de fechas generadas
-                // alert("Se generaron " + contadorFechas + " fechas.");
-
                 if (contadorFechas == 0) {
                     Swal.fire({
                         title: "Fechas No Generadas",
@@ -146,68 +122,56 @@
                         text: "Se generaron " + contadorFechas + " fechas.",
                         icon: "success"
                     });
-
-
                     var button = document.getElementById('btnEnviarFechas')
                     button.classList.remove('d-none');
                     hr.classList.remove('d-none');
-
-
-
-                    // Almacenar todas las fechas generadas en el input oculto
                     fechasGeneradasInput.value = JSON.stringify(fechasGeneradas);
-
-
-
-                    // Obtener las fechas generadas del input oculto
                     var fechasGeneradasInput = document.getElementById('fechas_generadas');
                     var fechasGeneradas = JSON.parse(fechasGeneradasInput.value);
-
-
-
                     contenedor.value = selectInput.value;
                     var obtenido = document.getElementById('funcionario');
                     var funcionario = JSON.parse(obtenido.value);
+                    var calendarEl = document.getElementById('calendario');
+                    var calendar = new FullCalendar.Calendar(calendarEl, {
+                        initialView: 'dayGridMonth',
+                        events: fechasGeneradas.map(function(fecha, index) {
+                            return {
+                                title: 'Evento',
+                                start: fecha,
+                                allDay: true,
+                                locale: 'es',
+                                inputId: "fecha" + index,
+                            };
+                        }),
+                        editable: true,
+                        eventDrop: function(info) {
+                            var inputId = info.event.extendedProps.inputId;
+                            var nuevaFecha = info.event.start;
+                            document.getElementById(inputId).value = nuevaFecha.toISOString().substr(0, 10);
+                            var inputNumero = parseInt(inputId.replace('fecha', ''));
+                            var indice = fechasGeneradas.indexOf(fechasGeneradas[inputNumero]);
+                            fechasGeneradas[indice] = nuevaFecha.toISOString().substr(0, 10);
+                            Swal.fire({
+                                height: 200,
+                                position: "top-end",
+                                icon: "success",
+                                title: "Fecha Actualizada",
+                                showConfirmButton: false,
+                                timer: 2500
+                            });
+                            fechasGeneradasInput.value = JSON.stringify(fechasGeneradas);
 
-
-                    // Verificar si todas las fechas son válidas
+                        }
+                    });
+                    calendar.render();
                     var fechasValidas = true;
                     fechasGeneradas.forEach(function(fecha) {
                         var date = new Date(fecha);
                         if (isNaN(date.getTime())) {
-                            // La fecha no es válida
                             fechasValidas = false;
                         }
                     });
-
-
-
-                    var calendarEl = document.getElementById('calendario');
-                    var calendar = new FullCalendar.Calendar(calendarEl, {
-                        // Opciones del calendario
-                        initialView: 'dayGridMonth', // Vista inicial del calendario (puedes cambiarla según tus necesidades)
-                        // Otros ajustes de configuración del calendario...
-
-                        // Agregar eventos al calendario
-                        events: fechasGeneradas.map(function(fecha) {
-                            return {
-                                title: 'Evento', // Título del evento
-                                start: fecha, // Fecha de inicio del evento
-                                allDay: true,
-                                locale: 'es' // Indica que el evento dura todo el día
-                                // Otros atributos del evento...
-                            };
-                        })
-                    });
-
-                    // Renderizar el calendario
-                    calendar.render();
-
-
-
-
                     if (fechasValidas) {
-                        // Todas las fechas son válidas, enviarlas al servidor
                         var xhr = new XMLHttpRequest();
                         xhr.open('POST', '/guardar-fechas');
                         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -218,16 +182,12 @@
                                 console.error('Error al guardar las fechas');
                             }
                         };
-                        // Crear el objeto con los datos a enviar al servidor
                         var datos = {
                             fechas: fechasGeneradas,
-                            funcionario: selectInput.value // Incluir el valor seleccionado del select
+                            funcionario: selectInput.value
                         };
-
-                        // Enviar los datos al servidor
                         xhr.send(JSON.stringify(datos));
                     } else {
-                        // Al menos una de las fechas no es válida, mostrar un mensaje de error
                         console.error('Una o más fechas no son válidas');
                     }
 
